7260acc78744da37a932f3efc4d8492cc86c198a
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-10-30 15:40:00 +0600

    updated

--- react-quiz/src/components/Layout.js
+++ react-quiz/src/components/Layout.js
@@ -1,5 +1,12 @@
+import classes from '../styles/Layout.module.css';
 import Nav from './Nav';
-export default function Layout() {
-    <Nav />
-  return <div></div>;
+export default function Layout({ children }) {
+  return (
+    <>
+      <Nav />
+      <main className={classes.main}>
+        <div className={classes.container}>{children}</div>
+      </main>
+    </>
+  );
 }
