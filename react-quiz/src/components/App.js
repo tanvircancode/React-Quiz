478d5757173761fe0af1a70ad2b672635d4ec635
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-11-01 21:03:38 +0600

    updated

--- react-quiz/src/components/App.js
+++ react-quiz/src/components/App.js
@@ -1,13 +1,14 @@
 import '../styles/App.css';
 import Layout from './Layout';
-import Signup from './pages/Signup';
+// import Signup from './pages/Signup';
 // import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
 
 function App() {
   return (
     <Layout>
-      <Signup />
       {/*<Login />*/}
+      <Quiz />  
     </Layout>
   );
 }
--- react-quiz/src/components/pages/Signup.js
+++ react-quiz/src/components/pages/Signup.js
@@ -27,7 +27,9 @@ export default function Signup() {
 
           <Checkbox text="I agree to the Terms &amp; Conditions" />
 
-          <Button>Submit Now </Button>
+          <Button>
+            <span>Submit Now </span>
+          </Button>
           <div className="info">
             Already have an account? <a href="login.html">Login</a> instead.
           </div>
