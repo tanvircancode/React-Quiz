26a1268d2771e910ab10074814457688795c5a1e
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-11-03 14:34:05 +0600

    updated

--- react-quiz/src/contexts/AuthContext.js
+++ react-quiz/src/contexts/AuthContext.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import '../firebase';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 
 const AuthContext = React.createContext();
 
-export function AuthProvider({children}) {
-    return (
-      <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
-    );
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
+  const [loading, setLoading] = useState(true);
+    const [currentUser, setCurrentUser] = useState();
+
+    //signup function
+    async function signup(email, password, username) {
+        const auth = getAuth();
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        // update profile
+        await updateProfile(auth.currentUser, {
+          displayName: username,
+        });
+
+        const user = auth.currentUser;
+        setCurrentUser({
+            ...user
+        });
+    }
+
+
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
