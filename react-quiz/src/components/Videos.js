da1c79b3a22828ab34b5070e5eaaa4ec26fc143f
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2021-11-06 23:24:36 +0600

    updated

--- react-quiz/src/components/Videos.js
+++ react-quiz/src/components/Videos.js
@@ -1,14 +1,45 @@
+import { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { Link } from 'react-router-dom';
-import classes from '../styles/Videos.module.css';
+import useVideoList from '../hooks/useVideoList';
 import Video from './Video';
 
 export default function Videos() {
-  return (
-    <div className={classes.videos}>
-      <Link to="/quiz">
-        <Video />
-      </Link>
+  const [page, setPage] = useState(1);
+  const { error, loading, videos, hasMore } = useVideoList(page);
 
+  return (
+    <div>
+      {videos.length > 0 && (
+        <InfiniteScroll
+          dataLength={videos.length}
+          hasMore={hasMore}
+          next={() => setPage(page + 8)}>
+          {videos.map((video) =>
+            video.noq > 0 ? (
+              <Link to="/quiz" key={video.youtubeID}>
+                <Video
+                  id={video.youtubeID}
+                  title={video.title}
+                  noq={video.noq}
+                />
+              </Link>
+            ) : (
+              <Link to="/quiz" key={video.youtubeID}>
+                <Video
+                  id={video.youtubeID}
+                  title={video.title}
+                  noq={video.noq}
+                />
+              </Link>
+            )
+          )}
+        </InfiniteScroll>
+      )}
+
+      {!loading && videos.length === 0 && <div>No data found!</div>}
+      {error && <div>There was an error</div>}
+      {loading && <div>Loading</div>}
     </div>
   );
 }
--- react-quiz/src/hooks/useVideoList.js
+++ react-quiz/src/hooks/useVideoList.js
@@ -1,39 +1,53 @@
-import { get, getDatabase, orderByKey, query, ref } from 'firebase/database';
+import {
+  get,
+  getDatabase,
+  limitToFirst,
+  orderByKey,
+  query,
+  ref,
+  startAt,
+} from 'firebase/database';
 import { useEffect, useState } from 'react';
 
-export default function useVideoList() {
-
-    const [error, setError] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const [videos, setVideos] = useState([]);
-
+export default function useVideoList(page) {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [videos, setVideos] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
 
   useEffect(() => {
     async function fetchVideos() {
       //   database works
       const db = getDatabase();
       const videosRef = ref(db, 'videos');
-        const videoQuery = query(videosRef, orderByKey());
+      const videoQuery = query(
+        videosRef,
+        orderByKey(),
+        startAt('' + page),
+        limitToFirst(8)
+      );
 
-        try {
-            setError(false);
-            setLoading(true);
-            // request firebase database
-            const snapshot = await get(videoQuery);
-            console.log(snapshot);
-            setLoading(false);
-            if (snapshot.exists()) {
-                setVideos((prevVideos) => {
-                    return [...prevVideos, Object.values(snapshot.val())]
-                })
-            } else {
-
-            }
-        } catch (err) {
-            console.log(err);
-            setLoading(false);
-            setError(true);
+      try {
+        setError(false);
+        setLoading(true);
+        // request firebase database
+        const snapshot = await get(videoQuery);
+        // console.log(snapshot);
+        setLoading(false);
+        if (snapshot.exists()) {
+          setVideos((prevVideos) => {
+            return [...prevVideos, ...Object.values(snapshot.val())];
+          });
+        } else {
+          setHasMore(false);
         }
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+        setError(true);
+      }
     }
-  }, []);
+    fetchVideos();
+  }, [page]);
+  return { error, loading, videos, hasMore };
 }
